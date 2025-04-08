521e4785e40fdf5abd9828c57869c848d54e5725
ArpitVadariya <arpit092vadariya@gmail.com>
2025-04-08 07:39:28 +0530

    bcrypt function use

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const port = 3000;
+const bcrypt = require("bcrypt");
 
-app.get("/", function (req, res) {
-  res.cookie("name", "ARPIT");
-  console.log("server started");
-
-  res.send("Done");
+app.get("/", (req, res) => {
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash("myPlaintextPassword", salt, function (err, hash) {
+      console.log(hash);
+    });
+  });
+  res.send("Hello World!");
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
