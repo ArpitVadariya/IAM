376340593c55907ba2cedd3ba8d1834ce32396ca
ArpitVadariya <arpit092vadariya@gmail.com>
2025-04-10 06:58:33 +0530

    jwt token store in cookies

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  bcrypt.compare(
-    "myPlaintextPassword",
-    "$2b$10$hrIHrp8rBW7VUALlboMNKevFwrWH/3WENAc339VPc1gPTsEZzZISK",
-    function (err, result) {
-      console.log(result);
-    }
-  );
+  let token = jwt.sign({ email: "arpit@example.com" }, "secretkey");
+  console.log(token);
+  res.cookie("token", token);
   res.send("Hello World!");
+});
+
+app.get("/read", function (req, res) {
+  // console.log(`Token is ${req.cookies.token}`);
+  let data = jwt.verify(req.cookies.token, "secretkey");
+  console.log(data);
+
+  res.send("Read Page");
 });
 
 app.listen(port, () => {
