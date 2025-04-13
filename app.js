d038dbe6376cbf179bdf23dcaca1463c6b287504
ArpitVadariya <arpit092vadariya@gmail.com>
2025-04-13 15:48:30 +0530

    user created in DB with hash password

--- app.js
+++ app.js
@@ -2,17 +2,40 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const bcrypt = require("bcrypt");
+
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
-app.set("view-engine", "ejs");
+const userModel = require("./models/user");
+const { error } = require("console");
+
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.send("Welcome Home");
+  res.render("index");
+});
+
+app.post("/create", (req, res) => {
+  let { username, email, password, age } = req.body;
+
+  bcrypt.genSalt(10, (error, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      console.log(hash);
+      let createdUser = await userModel.create({
+        username,
+        email,
+        password: hash,
+        age,
+      });
+
+      res.send(createdUser);
+    });
+  });
 });
 
 app.listen(port, () => {
