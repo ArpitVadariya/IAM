ea3fb32e50da823e24d063617c8b1467cadb9a4e
ArpitVadariya <arpit092vadariya@gmail.com>
2025-04-13 16:42:59 +0530

    Login and Logout Route Created

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const userModel = require("./models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const cookieParser = require("cookie-parser");
 const path = require("path");
-
-const userModel = require("./models/user");
-const { error } = require("console");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -16,16 +14,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
+// Home Route
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+// Create User Route
 app.post("/create", (req, res) => {
   let { username, email, password, age } = req.body;
 
   bcrypt.genSalt(10, (error, salt) => {
     bcrypt.hash(password, salt, async (err, hash) => {
-      console.log(hash);
+      console.log(`Password is ${hash}`);
       let createdUser = await userModel.create({
         username,
         email,
@@ -33,9 +33,41 @@ app.post("/create", (req, res) => {
         age,
       });
 
+      let token = jwt.sign({ email }, "secretkey");
+      console.log(`Token is ${token}`);
+      res.cookie("token", token);
+
       res.send(createdUser);
     });
   });
+});
+
+// Login Page Route For GET
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+// Login Page route For POST
+app.post("/login", async function (req, res) {
+  let user = await userModel.findOne({ email: req.body.email });
+  console.log(user);
+  if (!user) return res.send("something went wrong");
+  bcrypt.compare(req.body.password, user.password, function (err, result) {
+    console.log(result);
+    if (result) {
+      let token = jwt.sign({ email: user.email }, "secretkey");
+      console.log(`Token is ${token}`);
+      res.cookie("token", token);
+      res.send("YES You can login");
+    } else res.send("something went wrong");
+  });
+});
+
+// Logout Route
+app.get("/logout", function (req, res) {
+  res.cookie("token", "");
+  console.log("LOGUT SUCEESSFULLY");
+  res.redirect("/");
 });
 
 app.listen(port, () => {
